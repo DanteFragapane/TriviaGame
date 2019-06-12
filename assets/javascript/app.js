cde254c7f2bf87a782377b66b0dbe0c8ed9eb5a3
Dante Fragapane <dante.fragapane@gmail.com>
2019-06-12 16:50:05 -0400

    Removed the question object, it is no longer needed Adjusted the size of the question title and question text Added some comments describing code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,42 +6,38 @@ let maxTime = 30 // Maximum amount of seconds given to answer a question
 let timer = maxTime // Actual timer count
 let submitted = false
 
-const createQuestion = function createQuestion(number, question, options) {
-  return {
-    qNumber: number,
-    question: question,
-    options: options
-  }
-}
+// =======================================
+// Functions
+// =======================================
 
 // Creating the question view
-const createView = function createView(question) {
+const createView = function createView(qNumber, question, options) {
   let div = $('<div>', {
     class: 'question'
   })
-  div.append($('<h1>', {
-    text: 'Question ' + question.qNumber
+  div.append($('<h2>', {
+    text: 'Question ' + qNumber
   }))
-  div.append($('<h3>', {
+  div.append($('<h4>', {
     text: question
   }))
-  for (let i = 0; i < question.options.length; i++) {
-    let option = question.options[i]
+  for (let i = 0; i < options.length; i++) {
+    let option = options[i]
     if (option.match(/:answer/)) {
       option = option.replace(':answer', '')
       div.append($('<input>', {
         type: 'radio',
         text: option,
-        class: 'answerOption answer ',
-        name: question.qNumber,
+        class: 'answerOption answer',
+        name: qNumber,
         value: i
       }))
     } else {
       div.append($('<input>', {
         type: 'radio',
         text: option,
-        class: 'answerOption ',
-        name: question.qNumber,
+        class: 'answerOption',
+        name: qNumber,
         value: i
       }))
     }
@@ -53,31 +49,35 @@ const createView = function createView(question) {
   return div
 }
 
+// Update the timer's text
 const updateTimer = function updateTimer(seconds = timer) {
   if (seconds === 0) $('#timer').html('Time is Up!')
   else $('#timer').html(seconds + ' seconds left')
 }
 
-const submitForm = function submitForm(iputs, counter, timeout) {
+// Function to handle the submitted form's data
+const submitForm = function submitForm(inputs, counter, timeout) {
   clearInterval(counter)
   clearTimeout(timeout)
   updateTimer(0)
 
-  console.log(iputs)
+  console.log(inputs)
+
+  
 }
+// =======================================
+// =======================================
 
 // Settinng up the questions
 // ``createView`` takes three parameters: a question number, a title, the question, and an array of answer choices, with one needing a ':answer' suffix,
 // to program the answer in
-const question1 = createQuestion(1, 'What is this?', ['True:answer', 'False'])
-console.log(question1)
-const q1View = createView(question1)
+const question1 = createView(1, 'What is this?', ['True:answer', 'False'])
 const question2 = createView(2, 'What is that?', ['True', 'False:answer'])
 const question3 = createView(3, 'What is the fifth busies airport in the U.S. based on passenger enplanement?', ['JFK:answer', 'LAX', 'ORD', 'ATL'])
-const question4 = createView(4)
+const question4 = createView(4, 'What is the oldest continuous airport in operation in the United States?', ['KCLE', 'CYYZ', 'KCGS:answer'])
 
 // Creating an array with the questions inside
-const questionArray = [q1View]//, q2View, q3View, q4View]
+const questionArray = [question1, question2, question3, question4]
 const form = $('<form>')
 const submit = $('<button>', {
   id: 'submit',
@@ -101,8 +101,9 @@ let timeout = setTimeout(function () {
   if (!submitted) submitForm()
 }, maxTime * 1000)
 
+// On submitting the form
 $('#questions').on('submit', 'form', function (event) {
   event.preventDefault()
-  let inputs = $(this) //.serialize()
+  let inputs = $('input:checked')
   submitForm(inputs, counter, timeout)
 })
